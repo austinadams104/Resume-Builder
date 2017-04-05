5a7d5ed7bd8289870c027b6abcd2f760166d5490
Nathaniel Sneddon <nathaniel.sneddon@gmail.com>
2017-04-05 14:22:36 -0600

    Did some unsuccessful tests that will continue later

--- PHPTestFolder/databasetest.php
+++ PHPTestFolder/databasetest.php
@@ -1,8 +1,5 @@
-<? php
+<?php
+	
   require('../includes/phpfunctions.php');
 
-  $conn = connectToDatabase();
-  $sql = "SELECT user_id FROM user_accounts WHERE username='testuser';";
-  $results = $conn->query($sql);
-  echo $results;
  ?>

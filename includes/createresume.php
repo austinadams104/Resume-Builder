43e8b0d8d14fbbbac2f389dae43abcbd96ae138b
Nathaniel Sneddon <nathaniel.sneddon@gmail.com>
2017-04-21 15:42:35 -0600

    Now it adds files to the database :D

--- includes/createresume.php
+++ includes/createresume.php
@@ -9,14 +9,24 @@ if(!isset($_SESSION['user_id'])){
   $conn = connectToDatabase();
 
   $randomCode = md5($_SESSION['username'] + microtime());
-  $resume_name = "test";
+  $resume_name = "test"; //this is something that will somehow need to be passed in
 
 
   $sql = "INSERT INTO resumes(resume_name, file_path) VALUES ('" . $resume_name . "', '" . $randomCode . ".js');";
+	echo $sql . "<br>";
 
-  $sql = "select last_insert_id() from resumes;";
-  $resume_id = 4;
+  $resume_id = 0;
+  if ($conn->query($sql) === TRUE) {
+    $resume_id = $conn->insert_id;
+  } else {
+    die("Error: " . $conn->error);
+  }
 
-  $sql = "INSERT INTO permissions(user_id, permission, resume_id) VALUES (" . $_SESSION[user_id] . ", 3, " . $resume_id . ");";
+  $sql = "INSERT INTO permissions(user_id, permission, resume_id) VALUES (" . $_SESSION[user_id] . ", 1, " . $resume_id . ");";
   //We'll have to decide what permissions we should use
+  if ($conn->query($sql) === FALSE) {
+    die("Error: " . $conn->error);
+  } else{
+    echo "Record successfully inserted<br>Test it out<br>";
+  }
  ?>

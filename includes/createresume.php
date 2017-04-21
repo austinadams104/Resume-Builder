b3eb5b36c8bd455980cd5807e4e1c5862a9a989e
Nathaniel Sneddon <nathaniel.sneddon@gmail.com>
2017-04-21 14:58:13 -0600

    Forgot some semi colons

--- includes/createresume.php
+++ includes/createresume.php
@@ -9,14 +9,14 @@ if(!isset($_SESSION['user_id'])){
   $conn = connectToDatabase();
 
   $randomCode = md5($_SESSION['username'] + microtime());
-  $resume_name = "test"
+  $resume_name = "test";
 
 
   $sql = "INSERT INTO resumes(resume_name, file_path) VALUES ('" . $resume_name . "', '" . $randomCode . ".js');";
 
-  $sql = "select last_insert_id() from resumes;"
+  $sql = "select last_insert_id() from resumes;";
   $resume_id = 4;
 
-  $sql = "INSERT INTO permissions(user_id, permission, resume_id) VALUES (" . $_SESSION[user_id] . ", 3, " . $resume_id . ");"; 
+  $sql = "INSERT INTO permissions(user_id, permission, resume_id) VALUES (" . $_SESSION[user_id] . ", 3, " . $resume_id . ");";
   //We'll have to decide what permissions we should use
  ?>
